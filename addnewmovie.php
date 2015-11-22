6c66ee9da08ee1b74e901ae711e0cf9c377bc1b3
VIVTA <vivtan28@gmail.com>
2015-11-21 21:35:36 -0500

    Create addnewmovie.php

---
+++ addnewmovie.php
@@ -0,0 +1,34 @@
+  GNU nano 2.2.6             File: addnewmovie.php
+
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+   include 'connectdb.php';
+?>
+<h1>Here are your movies:</h1>
+<ol>
+<?php
+   $movieName= $_POST["MovieName"];
+   $movieID = $_POST["MovieID"];
+   $movieYear =$_POST["MovieYear"];
+   $query1='select max(movieID) as maxid from Movies';
+   $result=mysqli_query($connection,$query1);
+   if (!$result) {
+          die("database max query failed.");
+   }
+   $row=mysqli_fetch_assoc($result);
+   $newkey = intval($row["maxid"]) + 1;
+   $movieID = (string)$newkey;
+   $query = 'insert into Movies values("' . $movieID . '","' . $movieName . '","'$
+   if (!mysqli_query($connection, $query)) {
+        die("Error: insert failed" . mysqli_error($connection));
+    }
+   echo "Your movie was added!";
+   mysqli_close($connection);
+?>
+</ol>
+</body>
+</html>
+
+
