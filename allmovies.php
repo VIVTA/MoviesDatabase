9f5a424e762347fa77a5da7cd390952cec72ac17
VIVTA <vivtan28@gmail.com>
2015-11-21 22:31:54 -0500

    Update allmovies.php

--- allmovies.php
+++ allmovies.php
@@ -7,7 +7,8 @@ if (!$result) {
 }
 echo "<table><tr><th>Name</th><th>ID</th><th>Year</th></tr>";
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr><td>".$row["MovieName"]."</td><td>".$row["MovieID"]."</td><td>".$ro$
+    echo "<tr><td>".$row["MovieName"]."</td><td>".$row["MovieID"]."</td><td>".$row["Year"]."</td><tr>";
+
 }
 echo "</table>";
 mysqli_free_result($result);
