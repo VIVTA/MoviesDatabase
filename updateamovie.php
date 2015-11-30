adcc3737809e54089aade675281fd91a81ae6f73
VIVTA <vivtan28@gmail.com>
2015-11-30 01:12:10 -0500

    Create updateamovie.php

---
+++ updateamovie.php
@@ -0,0 +1,20 @@
+<?php
+   include 'connectdb.php';
+   $newyear = $_POST['movyear'];
+   $newtitle = $_POST['movtitle'];
+   $movieid = $_POST['movid'];
+   $query = 'Update Movies set MovieName="' . $newtitle . '", Year=' . $newyear .$
+   echo $query;
+   $result = mysqli_query($connection,$query);
+   if (!$result) {
+      die("Database query failed");
+   }
+   if (mysqli_query($connection, $query)) {
+      echo "Movie updated successfully";
+   } else {
+      echo "Error when updating movie: " . mysqli_error($connection);
+   }
+   mysqli_close($connection);
+?>
+
+
