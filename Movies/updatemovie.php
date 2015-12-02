8d2e713125f8ff2f950c96f93baafe4c10b5b051
VIVTA <vivtan28@gmail.com>
2015-12-02 14:48:54 -0500

    Update updatemovie.php

--- Movies/updatemovie.php
+++ Movies/updatemovie.php
@@ -8,14 +8,16 @@
 ?>
 
 <form  action="updateamovie.php" method="post">
-Movie Title: <input type="text" name="movtitle" value="<?php echo( htmlspecialcha$
+Movie Title: <input type="text" name="movtitle" value="<?php echo( htmlspecialchars( $movietitle)); ?>" />
 <br>
-Movie Year:  <input type="number" name="movyear" min="1910" max="2030" step="1" v$
+Movie Year:  <input type="number" name="movyear" min="1910" max="2030" step="1" value="<?php echo( htmlspecialchars( $movieyear)); ?>" />
 
-<input type="hidden" name="movid" value= "<?php echo( htmlspecialchars( $selected$
+<input type="hidden" name="movid" value= "<?php echo( htmlspecialchars( $selected_movie)); ?>" />
 
 <br>
 <input type="submit" value="Update this movie"><br>
 <hr>
 </body>
 </html>
+
+)
