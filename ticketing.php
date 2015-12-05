5d36745ef1974397252fdce977c75811ab3cf5db
Reid Vender <reid.vender@me.com>
2015-12-05 11:27:06 -0500

    Implemented portal

--- ticketing.php
+++ ticketing.php
@@ -7,23 +7,21 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="container">
-    <?php
-        include 'connectdb.php';
-    ?>
-    <h1>PSC - Ticket Desk</h1>
-    <hr>
-    <?php
-    	include 'inc/purchaseticket.inc.php';
-    	include 'inc/rateshowing.inc.php';
-    	include 'inc/filtershowings.inc.php';
-    	include 'inc/listviewedmovies.inc.php';
-    	include 'inc/viewcustomerprofile.inc.php';
-    	include 'inc/ticketsales.inc.php';
-    	include 'inc/moviespergenre.inc.php';
-    	include 'inc/topmovies.inc.php';
-    	mysqli_close($connection);
-    ?>
-</div>
+<?php
+    include 'connectdb.php';
+?>
+<h1>PSC - Ticket Desk</h1>
+<hr>
+<?php
+	include 'inc/purchaseticket.inc.php';
+	include 'inc/rateshowing.inc.php';
+	include 'inc/filtershowings.inc.php';
+	include 'inc/listviewedmovies.inc.php';
+	include 'inc/viewcustomerprofile.inc.php';
+	include 'inc/ticketsales.inc.php';
+	include 'inc/moviespergenre.inc.php';
+	include 'inc/topmovies.inc.php';
+	mysqli_close($connection);
+?>
 </body>
 </html>
